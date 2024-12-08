88f0e36df4770eaa034c547f08fc345f259eb601
mahdi baderloo <mahdi.baderloo.230@gmail.com>
2024-12-08 19:34:56 +0330

    coding header, login and cart template

---
+++ Scripts/app.js
@@ -0,0 +1,79 @@
+// DOM Elements //
+
+const categoryBtn = document.querySelector('.category')
+const categoryItems = document.querySelector('.category-items')
+const categoryAngle = document.querySelector('.category-angle')
+const burgerMenu = document.querySelector('.burger-menu')
+const closeNav = document.querySelector('.close-nav')
+const mobileNavbarBox = document.querySelector('.mobile-navbar-box')
+const mobileNavbar = document.querySelector('.mobile-navbar')
+const upBtn = document.querySelector('.up-button-box')
+
+
+// Category //
+
+function categoryItemsOpen () {
+    categoryItems.style.display = 'flex'
+    categoryAngle.style.transform = 'rotate(180deg)'
+}
+
+function categoryItemsClose () {
+    categoryItems.style.display = 'none'
+    categoryAngle.style.transform = 'rotate(0deg)'
+}
+
+categoryBtn.addEventListener('click', event => {
+    event.preventDefault()
+    
+    if (categoryItems.style.display === 'none') {
+        categoryItemsOpen()
+    } else {
+        categoryItemsClose()
+    }
+})
+
+
+// Mobile Navbar //
+
+burgerMenu.addEventListener('click', () => {
+    mobileNavbar.style.left = '0'
+})
+
+closeNav.addEventListener('click', () => {
+    mobileNavbar.style.left = '-81%'
+})
+
+
+// Up Button //
+
+upBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
+
+
+// fetch('https://api.escuelajs.co/api/v1/categories')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+
+
+
+
+// Window //
+
+window.addEventListener('click', event => {
+    if (event.target.className != 'category-items' && event.target.className != 'nav-link category') {
+        categoryItemsClose()
+    }
+})
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    if (window.scrollY >= 295) {
+        mobileNavbarBox.style.position = 'fixed'
+        upBtn.style.bottom = '1rem'
+    } else {
+        mobileNavbarBox.style.position = 'unset'
+        upBtn.style.bottom = '-3rem'
+    }
+
+})
---
+++ Scripts/change-language.js
@@ -0,0 +1,84 @@
+// Language Button Variables //
+
+const languageDisplay = document.querySelector('.language-display')
+const languageSelected = document.querySelector('.language-selected')
+const languageAngle = document.querySelector('.language-angle')
+const languageOptions = document.querySelector('.language-options')
+const languageLinks = document.querySelectorAll('.language-link')
+
+
+//  User's chosen language //
+
+languageDisplay.addEventListener('click', () => {
+
+    if (languageOptions.style.display === 'none') {
+        openLanguageBox()
+    } else {
+        closeLanguageBox()
+    }
+})
+
+function openLanguageBox () {
+    languageAngle.style.transform = 'rotate(180deg)'
+    languageOptions.style.display = 'flex'
+}
+
+function selectLanguage (data) {
+    let select = data
+    languageSelected.innerHTML = select
+
+    setInLocalStorage(data)
+    changeDOMlanguage(select)
+    closeLanguageBox()
+}
+
+function closeLanguageBox () {
+    languageAngle.style.transform = 'rotate(0deg)'
+    languageOptions.style.display = 'none'
+}
+
+languageLinks.forEach( item => {
+    item.addEventListener('click', event => {
+        event.preventDefault()
+        selectLanguage(event.target.dataset.language)
+    })
+})
+
+
+// Change DOM Language //
+
+function changeDOMlanguage (data) {
+    
+    if (data === 'Persian') {
+        document.location.href = 'app-persian.html'
+    }
+
+    if (data === 'English') {
+        document.location.href = 'index.html'
+    }
+
+}
+
+
+
+// Local Storage //
+
+function setInLocalStorage (data) {
+    let localData = {language: data}
+    localStorage.setItem('language', JSON.stringify(localData))
+}
+
+function getLocalData () {
+    let data = localStorage.getItem('language')
+    console.log(JSON.parse(data))
+}
+
+
+
+// Window's events //
+
+window.addEventListener('click', event => {
+    if (event.target.className != 'language-display' && event.target.className != 'language-selected' && event.target.id != 'language-angle') {
+        closeLanguageBox()
+    }
+})
