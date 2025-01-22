81d253638d44d08cce1e9902ff862804cf8e84ee
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-01-22 20:00:11 +0330

    persian items

--- Scripts/shopping-cart.js
+++ Scripts/shopping-cart.js
@@ -3,6 +3,10 @@
 let cartItems = document.querySelector('.cart-items')
 
 
+// Variables //
+
+let language = JSON.parse(localStorage.getItem('language'))
+
 
 // Get Items From Local Storage //
 
@@ -26,22 +30,42 @@ function addItemsToCart () {
 // Create Items //
 
 function createItem (item) {
-    cartItems.insertAdjacentHTML('beforeend' , `
-        <li class="cart-item">
-            <div class="product-content">
-                <img src="${item.image}" alt="item image" class="product-img">
-                <p class="product-title">${item.title.replace(",", "'")}</p>
-            </div>
-            <div class="count-box">
-                <p class="product-count">1</p>
-                <div class="change-count">
-                    <span class="count-plus">+</span>
-                    <span class="count-minus">-</span>
+    if (language.innerHTML == 'English') {
+        cartItems.insertAdjacentHTML('beforeend' , `
+            <li class="cart-item">
+                <div class="product-content">
+                    <img src="${item.image}" alt="item image" class="product-img">
+                    <p class="product-title">${item.title.replace(",", "'")}</p>
                 </div>
-            </div>
-            <p class="product-price">${item.price}$</p>
-            <img src="Images/delete-icon.svg" alt="delete icon" class="delete-product">
-        </li>`)
+                <div class="count-box">
+                    <p class="product-count">1</p>
+                    <div class="change-count">
+                        <span class="count-plus">+</span>
+                        <span class="count-minus">-</span>
+                    </div>
+                </div>
+                <p class="product-price">${item.price}$</p>
+                <img src="Images/delete-icon.svg" alt="delete icon" class="delete-product">
+            </li>`)
+    } else {
+        cartItems.insertAdjacentHTML('beforeend' , `
+            <li class="cart-item">
+                <div class="product-content">
+                    <img src="${item.image}" alt="item image" class="product-img">
+                    <p class="product-title">${item.persianTitle}</p>
+                </div>
+                <div class="count-box">
+                    <p class="product-count">1</p>
+                    <div class="change-count">
+                        <span class="count-plus">+</span>
+                        <span class="count-minus">-</span>
+                    </div>
+                </div>
+                <p class="product-price">${(item.price * 80_000).toLocaleString()}</p>
+                <img src="Images/delete-icon.svg" alt="delete icon" class="delete-product">
+            </li>`)
+
+    }
 }
 
 
