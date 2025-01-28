1701e780d982e7a36b291b47289c39b21b130f98
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-01-28 10:11:37 +0330

    login styles

---
+++ Scripts/login.js
@@ -0,0 +1,11 @@
+// Dom Elements //
+
+const loginTitle = document.querySelector('.login-title')
+const switchLink = document.querySelector('.signup-login-switch')
+const switchButton = document.querySelector('.switch')
+const loginNow = document.querySelector('.login-now')
+
+
+
+
+
