1e5bdff8f4cddd6728f6524c0139ba32f23f073e
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-01-28 10:35:41 +0330

    switch login/sign up persian

--- Scripts/login.js
+++ Scripts/login.js
@@ -8,6 +8,11 @@ const googleLogin = document.querySelector('.google-login-box')
 const facebookLogin = document.querySelector('.facebook-login-box')
 
 
+// Language //
+
+let language = JSON.parse(localStorage.getItem('language'))
+
+
 // Set user in local storage //
 
 function setUserInLocalStorage (username, password) {
@@ -23,19 +28,40 @@ function setUserInLocalStorage (username, password) {
 
 switchLink.addEventListener('click', event => {
     event.preventDefault()
-    if (loginTitle.textContent === 'LOGIN') {
-        loginTitle.innerHTML = 'SIGN UP'
-        loginNow.innerHTML = 'Sign Up Now'
-        switchTitle.innerHTML = ''
-        switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch">You have an account? <a href="" class="colorless signup-login-switch">Login</a></p>`)
-        googleLogin.style.display = 'none'
-        facebookLogin.style.display = 'none'
+
+    if (!language || language.language == 'English') {
+        if (loginTitle.textContent === 'LOGIN') {
+            loginTitle.innerHTML = 'SIGN UP'
+            loginNow.innerHTML = 'Sign Up Now'
+            switchTitle.innerHTML = ''
+            switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch">You have an account? <a href="" class="colorless signup-login-switch">Login</a></p>`)
+            googleLogin.style.display = 'none'
+            facebookLogin.style.display = 'none'
+        } else {
+            loginTitle.innerHTML = 'LOGIN'
+            loginNow.innerHTML = 'Login Now'
+            switchTitle.innerHTML = ''
+            switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch">Need an account? <a href="" class="colorless signup-login-switch">Sign Up</a></p>`)
+            googleLogin.style.display = 'flex'
+            facebookLogin.style.display = 'flex'
+        }
     } else {
-        loginTitle.innerHTML = 'LOGIN'
-        loginNow.innerHTML = 'Login Now'
-        switchTitle.innerHTML = ''
-        switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch">Need an account? <a href="" class="colorless signup-login-switch">Sign Up</a></p>`)
-        googleLogin.style.display = 'flex'
-        facebookLogin.style.display = 'flex'
+        if (loginTitle.textContent === 'ورود') {
+            loginTitle.innerHTML = 'ثبت نام'
+            loginNow.innerHTML = 'ثبت نام'
+            switchTitle.innerHTML = ''
+            switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch"> حساب داری؟ <a href="" class="colorless signup-login-switch">ورود</a></p>
+`)
+            googleLogin.style.display = 'none'
+            facebookLogin.style.display = 'none'
+        } else {
+            loginTitle.innerHTML = 'ورود'
+            loginNow.innerHTML = 'ورود'
+            switchTitle.innerHTML = ''
+            switchTitle.insertAdjacentHTML('afterbegin', `<p class="switch">یه حساب جدید میخوای؟ <a href="" class="colorless signup-login-switch">ثبت نام</a></p>
+`)
+            googleLogin.style.display = 'flex'
+            facebookLogin.style.display = 'flex'
+        }
     }
 })
